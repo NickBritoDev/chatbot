2155616ac6dfd7599252a68a67b5343a1a930254
NickBritoDev <nicolasbcruz@gmail.com>
2023-12-28 13:24:22 -0300

    update for ios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import 'react-chat-widget/lib/styles.css'
+import '@ionic/react/css/core.css'
 import '@fontsource/poppins'
 import React from 'react'
 import Router from './routers'
+import { setupIonicReact } from '@ionic/react'
 
+setupIonicReact()
 export default function App () {
   return <Router />
 }
